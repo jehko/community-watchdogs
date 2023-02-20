58238440b508c3da42e62ac853a8e05e7580124b
Jehko <krelude.jko@gmail.com>
2023-02-21 01:43:08 +0900

    [Init]project created

---
+++ src/main/java/com/refactory/communitywatchdogs/CommunityWatchdogsApplication.java
@@ -0,0 +1,13 @@
+package com.refactory.communitywatchdogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityWatchdogsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityWatchdogsApplication.class, args);
+    }
+
+}
